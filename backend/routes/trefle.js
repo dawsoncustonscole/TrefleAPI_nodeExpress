2dff81b25dba8146a9cdb4766e5297d19d441e32
Alex Dawson <alexecdawson@gmail.com>
2021-04-23 17:44:17 +1000

    make trefle api request and return token

---
+++ backend/routes/trefle.js
@@ -0,0 +1,19 @@
+const axios = require('axios');
+
+//THIS RETURNS PENDING PROMISE
+
+const getToken = async () => {
+    const params = {
+        token: '9_oZxBqmEGIMKQJqdyns9ZsrfESdbhlrrkEV-H_hkss',
+        origin: 'http://localhost:5000',
+    };
+
+    const response = await axios.post(
+        'https://trefle.io/api/auth/claim',
+        params
+    );
+
+    return response.data;
+};
+
+module.exports = { getToken };
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const axios = require('axios');
+const trefleAPI = require('./routes/trefle');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('API is running...');
+app.get('/', async (req, res) => {
+    res.send('Server running on port 5000');
+});
+
+app.get('/api', async (req, res) => {
+    const value = await trefleAPI.getToken();
+
+    console.log(value);
+
+    res.send(value);
 });
 
 app.listen(5000, console.log('Server running on port 5000'));
