ae1e14c855cb4aafefc0db431df2df8e8b2f0bd4
Alex Dawson <alexecdawson@gmail.com>
2021-04-26 13:52:30 +1000

    add trefle client request, hardcode search term

--- backend/routes/trefle.js
+++ backend/routes/trefle.js
@@ -1,20 +1,36 @@
 const axios = require('axios');
 
-//THIS RETURNS PENDING PROMISE
-
 const getToken = async () => {
     const params = {
         token: '9_oZxBqmEGIMKQJqdyns9ZsrfESdbhlrrkEV-H_hkss',
         origin: 'http://localhost:5000',
     };
 
-    const response = await axios.post(
-        'https://trefle.io/api/auth/claim',
-        params
-    );
+    const response = await axios
+        .post('https://trefle.io/api/auth/claim', params)
+        .catch((error) => {
+            console.log('error', error.response);
+        });
 
     console.log('token requested from trefle.js');
-    return response.data;
+    return response.data.token;
 };
 
-module.exports = { getToken };
+//TO DO add axios.create method to open connection using revceived key
+
+const getData = async (token, term) => {
+    const response = await axios
+        .get('https://trefle.io/api/v1/plants/search', {
+            params: {
+                token: token,
+                q: term,
+            },
+        })
+        .catch((error) => {
+            console.log('error', error.response);
+        });
+
+    return response.data.data;
+};
+
+module.exports = { getToken, getData };
--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,10 @@ const trefleAPI = require('./routes/trefle');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Server running on port 5000');
-});
-
-app.get('/api/token', async (req, res) => {
+app.get('/', async (req, res) => {
     const value = await trefleAPI.getToken();
-    res.json(value);
+    const data = await trefleAPI.getData(value, 'Rose');
+    res.send(data);
 });
 
 app.listen(5000, console.log('Server running on port 5000'));
